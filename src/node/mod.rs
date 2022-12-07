d658738305a46a977a1e4521e60b7cc9e9918cd7
Che Kwas <che.kwas@outlook.com>
2022-12-07 17:48:25 +0800

    wip: add graceful shutdown

--- src/node/mod.rs
+++ src/node/mod.rs
@@ -7,6 +7,7 @@ use axum::{
 };
 use log::{debug, info};
 use std::net::SocketAddr;
+use tokio::signal;
 
 use crate::{database::*, utils};
 
@@ -25,10 +26,35 @@ pub async fn run(ip: &str, port: u16, miner: &str) -> Result<(), NodeError> {
     info!("Listening on {}", socket_addr);
     axum::Server::bind(&socket_addr)
         .serve(app.into_make_service())
+        .with_graceful_shutdown(shutdown_signal())
         .await
         .unwrap();
 
     Ok(())
+}
+
+async fn shutdown_signal() {
+    let ctrl_c = async {
+        signal::ctrl_c()
+            .await
+            .expect("failed to install Ctrl+C handler");
+    };
+
+    #[cfg(unix)]
+    let terminate = async {
+        signal::unix::signal(signal::unix::SignalKind::terminate())
+            .expect("failed to install signal handler")
+            .recv()
+            .await;
+    };
+
+    #[cfg(not(unix))]
+    let terminate = std::future::pending::<()>();
+
+    tokio::select! {
+        _ = ctrl_c => {},
+        _ = terminate => {},
+    }
 }
 
 async fn hello() -> Result<(), NodeError> {
