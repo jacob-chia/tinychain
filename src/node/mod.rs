919553b1ea5d31885fd84840a5b347e2218c40ae
Che Kwas <che.kwas@outlook.com>
2022-12-11 17:02:24 +0800

    refactor: state

--- src/node/mod.rs
+++ src/node/mod.rs
@@ -1,6 +1,6 @@
 use axum::Server;
 use ethers_core::types::H256;
-use log::{error, info};
+use log::info;
 use std::{
     collections::{HashMap, HashSet},
     net::SocketAddr,
@@ -10,12 +10,8 @@ use tokio::signal;
 
 mod router;
 
-use crate::{
-    database::{self, Block, SignedTx, State, Tx},
-    error::ChainError,
-};
+use crate::{database::*, error::ChainError, types::Hash};
 
-// 挖矿难度
 const MINING_DIFFICULTY: usize = 3;
 
 #[derive(Debug)]
@@ -46,9 +42,9 @@ impl Node {
         let addr = self.addr;
         info!("Listening on {addr}");
         info!("Current state =====================================");
-        info!("balances         : {:?}", self.state.get_balances());
+        info!("balances         : {:?}", self.get_balances());
         info!("latest_block     : {:?}", self.state.latest_block());
-        info!("latest_block_hash: {:?}", self.state.latest_block_hash());
+        info!("latest_block_hash: {:?}", self.latest_block_hash());
 
         let app = router::new_router(Arc::new(RwLock::new(self)));
 
@@ -59,7 +55,7 @@ impl Node {
             .unwrap();
     }
 
-    pub fn add_tx(&mut self, from: &str, to: &str, value: u64) {
+    pub fn add_tx(&mut self, from: &str, to: &str, value: u64) -> Result<(), ChainError> {
         let next_nonce = self.state.next_account_nonce(from);
         let tx = Tx::builder()
             .from(from)
@@ -69,11 +65,7 @@ impl Node {
             .build()
             .sign();
 
-        self.add_pending_tx(tx, self.addr);
-    }
-
-    pub fn add_peer(&mut self, peer: &str) {
-        self.peers.insert(peer.parse().unwrap());
+        self.add_pending_tx(tx, self.addr)
     }
 
     pub fn get_pending_txs(&self) -> Vec<SignedTx> {
@@ -83,13 +75,38 @@ impl Node {
             .collect::<Vec<SignedTx>>()
     }
 
-    fn add_pending_tx(&mut self, tx: SignedTx, from_peer: SocketAddr) {
-        if tx.check_signature().is_err() {
-            return;
-        }
+    pub fn add_peer(&mut self, peer: &str) -> Result<(), ChainError> {
+        self.peers.insert(peer.parse()?);
 
+        Ok(())
+    }
+
+    pub fn get_blocks(&self, from_height: usize) -> Vec<Block> {
+        self.state.get_blocks(from_height)
+    }
+
+    pub fn get_block(&self, number: u64) -> Result<Block, ChainError> {
+        self.state.get_block(number)
+    }
+
+    pub fn get_balances(&self) -> HashMap<String, u64> {
+        self.state.get_balances()
+    }
+
+    pub fn latest_block_hash(&self) -> Hash {
+        self.state.latest_block_hash()
+    }
+
+    pub fn latest_block_number(&self) -> u64 {
+        self.state.latest_block_number()
+    }
+
+    fn add_pending_tx(&mut self, tx: SignedTx, from_peer: SocketAddr) -> Result<(), ChainError> {
+        tx.check_signature()?;
         info!("Added pending tx {:?} from peer {}", tx, from_peer);
         self.pending_txs.entry(tx.hash()).or_insert(tx);
+
+        Ok(())
     }
 
     fn connect_to_peer(&mut self, peer: &str) {
